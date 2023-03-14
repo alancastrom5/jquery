f2403262686e367cb39f57a088dc7006fba0031c
alancastrom5 <79289644+alancastrom5@users.noreply.github.com>
2023-03-13 21:34:08 -0300

    up

---
+++ script.js
@@ -0,0 +1,19 @@
+// lacos 
+// var i =0;
+// while (i < 3){
+//     alert(i);
+//     i = i=1;
+// }
+
+// for (var j = 0; j < 3; j++){
+//     alert(j)
+// }
+// tipos de dados 
+// var numero = 6;
+// var decimal = 4.5;
+// var  texto = "Ola mundo";
+// var lista = ["laranja", "maca", "banana"];
+// // array 
+// for (i in lista){
+//     alert(lista[i]);
+// }
---
+++ scriptJquery.js
@@ -0,0 +1,53 @@
+// $(document).ready(function(){
+//     $('button').click(function(){// clica no botao e a div e escondida
+//         $('h1').hide();//esconder div jquery
+//     });
+// });
+
+// $(function(){
+//         $('#azul').click(function(){// clica no botao e a div e escondida
+//             $('p').css('font-size','20vw');//esconder div jquery
+//             $('p').fadeOut();
+//             $('p').delay(3000);
+//             $('p').fadeIn();
+//         });
+//         $('#vermelho').click(function(){// clica no botao e a div e escondida
+//             $('p').css('font-size','10vw');//esconder div jquery
+//             $('p').fadeOut('slow');
+//             $('p').delay(3000);
+//             $('p').fadeIn('slow');
+//         });
+//     });
+// gravando mensagem 
+// $(function(){
+//         $('#azul').click(function(){// clica no botao e a div e escondida
+//             $('p').css('font-size','20vw');//esconder div jquery
+//             $('p').fadeOut();
+//             $('p').delay(3000);
+//             $('p').fadeIn();
+//         });
+//         $('#vermelho').click(function(){// clica no botao e a div e escondida
+//         $('p').css('font-size','10vw');//esconder div jquery
+//         $('#mensagem').text('tamanho alterado com sucesso');
+//         $('#mensagem').css('color','red');
+//         $('#mensagem').css('border','1px solid');
+//         $('#mensagem').fadeOut(3000);
+//     });
+// });
+//encadeamento em bloco
+// $(function(){
+//         $('#azul').click(function(){// clica no botao e a div e escondida
+//             $('p').css('font-size','20vw')//esconder div jquery
+//             .fadeOut()
+//             .delay(3000)
+//             .fadeIn();
+//         });
+//         $('#vermelho').click(function(){// clica no botao e a div e escondida
+//         $('p').css('font-size','10vw');//esconder div jquery
+//         $('#mensagem')
+//         .text('tamanho alterado com sucesso')
+//         .css('color','red')
+//         .css('border','1px solid')
+//         .fadeOut(3000);
+//     });
+// });
